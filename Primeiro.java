48119679a034f3a3b98579b5817a1e02710e1dcd
GilsonRodriguesAlmeida <gilson.almeida@al.infnet.edu.br>
2021-05-13 20:43:30 -0300

    Apresentação Projeto em Java.

--- Primeiro.java
+++ Primeiro.java
@@ -9,43 +9,53 @@ public class Primeiro{
     private static boolean ehJava;
      
     private static void tratarParametros(){
-        String nome = parametros[0];
-	String sobrenome = parametros[1];
+        nome = parametros[0];
+	sobrenome = parametros[1];
 	idade = Integer.valueOf(parametros[2]);
 	salario = Float.valueOf(parametros[3]);
 	qtdeMeses = Integer.valueOf(parametros[5]);
 	ehJava = "S".equalsIgnoreCase(parametros[4]) ? true : false; //-> estrutura ternÃ¡rio.
     }
     
-    private static boolean validar(){
-        if(args.length ==6){
-            return true;
-        }
-        return false;
+    private static boolean validar(int qtde){
+        return qtde == 6;
+    }
+    
+    private static int definirAnoNascimento(){
+        return 2021 - idade;
+    }
+    
+    private static float calcularValorTotal(){
+        return qtdeMeses * salario;
+    }
+    
+    private static String obterPerfilPorIdade(){
+        return idade < 50 ? "iniciante" : "veterano";
+    }
+    
+    private static String obterPerfilPorSalario(){
+        return salario > 1000 ? "Estável" : "Instável";
+    }
+    
+    private static void imprimir(){
+        
+        System.out.println("Nome: " + nome);
+        System.out.println("Sobrenome: " + sobrenome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Nascimento: " + definirAnoNascimento());
+        System.out.println("SalÃ¡rio: " + salario);
+        System.out.println("Java? " + ehJava);
+        System.out.println("Salario Total: " + calcularValorTotal());
+        System.out.println("Nivel: " + obterPerfilPorIdade());
+        System.out.println("Situação: " + obterPerfilPorSalario());
     }
     
     public static void main(String[] args){
 	
-	if(validar()){
+	if(validar(args.length)){
             parametros = args;
-            
             tratarParametros();
-            
-            int anoNascimento = 2021 - idade;
-            float salarioTotal = salario * qtdeMeses;
-
-            String nivel = idade < 50 ? "iniciante" : "veterano";
-            String situacao = salario > 1000 ? "estavel" : "instavél";
-
-            System.out.println("Nome: " + nome);
-            System.out.println("Sobrenome: " + sobrenome);
-            System.out.println("Idade: " + idade);
-            System.out.println("Nascimento: " + anoNascimento);
-            System.out.println("SalÃ¡rio: " + salario);
-            System.out.println("Java? " + ehJava);
-            System.out.println("Salario Total: " + salarioTotal);
-            System.out.println("Nivel: " + nivel);
-            System.out.println("Situação: " + situacao);
+            imprimir();
 	} else {
             System.out.println("Problemas na quantidade de parametros.");
             }
